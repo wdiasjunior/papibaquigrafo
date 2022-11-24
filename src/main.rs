35e7cf31713c7c38ba1d98c34d426d0e90407ed9
Wellington Junior <wdiasjunior@gmail.com>
2022-11-24 11:50:17 -0300

    updated TODO

--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,10 @@ fn main() {
     - eye candy -> papibaquigrafo ascii logo and colors?
     - input -> select mangadex or tcbScans
       - mangadex -> user enters manga id (implement search function in the future?)
-      - tcbScans -> user selects which manga to donwload
+      - tcbScans -> user selects which manga to download
     - progress bar
     - chapter selection
-    -
+    - implement tcbScans scraper
+      - selection of manga titles
   - generalized String arguments from this video -> https://www.youtube.com/watch?v=b0bgAYJDhhQ
 */
