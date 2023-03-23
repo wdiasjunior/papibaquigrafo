4e4ccc935b58b2406254d3c9a331d238efd9a91a
Wellington Junior <wdiasjunior@gmail.com>
2023-03-23 20:10:25 -0300

    tcb download. still need chapter selection input

--- src/tcbscans.rs
+++ src/tcbscans.rs
@@ -7,15 +7,13 @@ use std::io::{self, Write};
 
 // use indicatif::ProgressBar;
 
-type MangaList = Vec<String>;
-
-fn getMangaList() -> MangaList {
+fn getMangaList() -> Vec<String> {
   let baseUrl = "https://onepiecechapters.com/projects";
   let url = reqwest::Url::parse(&baseUrl).unwrap();
   let json = reqwest::blocking::get(url).expect("bad request").text().unwrap();
 
-  let mangaList: MangaList = Regex::new(r#"/mangas/(.*?)[^""]+"#).unwrap().find_iter(&json).map(|e| e.as_str().to_string()).collect();
-  let mut filteredMangaList: MangaList = Vec::new();
+  let mangaList: Vec<String> = Regex::new(r#"/mangas/(.*?)[^""]+"#).unwrap().find_iter(&json).map(|e| e.as_str().to_string()).collect();
+  let mut filteredMangaList: Vec<String> = Vec::new();
   let mut n = 0;
   print!("Enter the Manga ID: \n");
   for manga in &mangaList {
@@ -28,9 +26,7 @@ fn getMangaList() -> MangaList {
   return filteredMangaList;
 }
 
-type ChapterList = Vec<String>;
-
-fn getChapterList(_manga: String) -> ChapterList {
+fn getChapterList(_manga: String) -> Vec<String> {
   print!("\x1B[2J\x1B[1;1H"); // clears terminal
   print!("{} \n\n", _manga);
 
@@ -38,11 +34,55 @@ fn getChapterList(_manga: String) -> ChapterList {
   let url = reqwest::Url::parse(&baseUrl).unwrap();
   let json = reqwest::blocking::get(url).expect("bad request").text().unwrap();
 
-  let chapterList: ChapterList = Regex::new(r#"/chapters/(.*?)[^""]+"#).unwrap().find_iter(&json).map(|e| e.as_str().to_string()).collect();
+  let chapterList: Vec<String> = Regex::new(r#"/chapters/(.*?)[^""]+"#).unwrap().find_iter(&json).map(|e| e.as_str().to_string()).collect();
   for chapter in &chapterList {
     print!("{}\n", chapter);
   }
   return chapterList;
+}
+
+fn getChapterImages(_mangaTitle: String, _mangaChapter: String) {
+  print!("\x1B[2J\x1B[1;1H"); // clears terminal
+  print!("{} \n\n", _mangaChapter);
+
+  let baseUrl = format!("https://onepiecechapters.com{}", &_mangaChapter);
+  let url = reqwest::Url::parse(&baseUrl).unwrap();
+  let json = reqwest::blocking::get(url).expect("bad request").text().unwrap();
+
+  let chapterImages: Vec<String> = Regex::new(r#"https://cdn.onepiecechapters.com/file/(.*?)[^""]+"#).unwrap().find_iter(&json).map(|e| e.as_str().to_string()).collect();
+
+  let mangaTitle: Vec<String> =  Regex::new(r#"[^/]*$"#).unwrap().find_iter(&_mangaTitle).map(|e| e.as_str().to_string().replace("-", " ")).collect();
+  let mangaChapter: Vec<String> = Regex::new(r#"\d+$"#).unwrap().find_iter(&_mangaChapter).map(|e| e.as_str().to_string()).collect();
+
+  // print!("mangaTitle {}\n", mangaTitle[0]);
+  // print!("mangaChapter {}\n", mangaChapter[0]);
+
+  let directory = format!("downloads/{}/Ch.{}/", mangaTitle[0], mangaChapter[0]);
+  std::fs::create_dir_all(&directory).unwrap();
+
+  let mut i = 0;
+  for image in &chapterImages {
+    print!("{}\n", image);
+    let fileExtension = if image.contains(".jpg") {
+      "jpg"
+    } else if image.contains(".jpeg") {
+      "jpeg"
+    } else if image.contains(".png") {
+      "png"
+    } else {
+      "webp"
+    };
+    let fileName = if i + 1 < 10 {
+      format!("{}/00{}.{}", &directory, i + 1, fileExtension)
+    } else {
+      format!("{}/0{}.{}", &directory, i + 1, fileExtension)
+    };
+    let mut file = std::fs::File::create(fileName).unwrap();
+    // let baseUrl = format!("https://uploads.mangadex.org/data/{}/{}", hash, chapterImagesFileName[j]);
+    let url = reqwest::Url::parse(&image).unwrap();
+    let _mangaImage = reqwest::blocking::get(url).unwrap().copy_to(&mut file).unwrap();
+    i += 1;
+  }
 }
 
 pub fn tcbscans() {
@@ -90,11 +130,14 @@ pub fn tcbscans() {
     // break;
   // }
   let index: usize = userInput.trim().parse().unwrap();
+  let _mangaTitle = _mangaList[index].clone();
   let _chapterList = getChapterList(_mangaList[index].clone());
   // getChapterList(_mangaList[index].clone());
 
   // print!("\x1B[2J\x1B[1;1H"); // clears terminal
   for chapter in _chapterList {
-    print!("{}\n", chapter);
+    // print!("{}\n", chapter);
+    getChapterImages(_mangaTitle.clone(), chapter);
+    // getChapterImages(_mangaTitle, _chapterList[0].clone());
   }
 }
