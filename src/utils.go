7e1818d851012378ccd0cce49c3ee94bfa0c5859
Wellington Junior <wdiasjunior@gmail.com>
2023-07-10 01:02:58 -0300

    utils

--- src/utils.go
+++ src/utils.go
@@ -8,3 +8,12 @@ func PrettyPrintJson(_json interface{}) string {
   s, _ := json.MarshalIndent(_json, "", "\t")
   return string(s)
 }
+
+func contains(slice []string, element string) bool {
+  for _, el := range slice {
+    if el == element {
+      return true
+    }
+  }
+  return false
+}
